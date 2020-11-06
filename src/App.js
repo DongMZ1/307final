7d52dcacb4b851d9b101ead505ee565ec8d11212
xren935 <xingya.ren@mail.mcgill.ca>
2020-11-05 23:55:09 -0500

    styled header + added carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< Updated upstream
 import IndexPage from './IndexPage';
+=======
+import Header from './Header'
+import Gallery from './Gallery'
+import Footer from './Footer'
+import HomePage from './HomePage'
+import AboutContacts from './About/AboutContacts'
+import AcademicOverview from './Academic/AcademicOverview'
+import Donate from './Donate/Donate'
+import EmploymentFaculty from './Employment/EmploymentFaculty'
+import News from './News/News'
+import PeopleFaculty from './People/PeopleFaculty'
+>>>>>>> Stashed changes
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <IndexPage />
+=======
+      <Router>
+      <Header />
+      <Gallery />
+       <Switch>
+         {/*homepage*/}
+           <Route path = '/' component = {HomePage} exact />
+
+         {/*prospectivePages*/}
+
+           <Route path = "/ProspectivePages/ProspectiveGeneralInfo" component = {ProspectiveGeneralInfo} />
+           <Route path = "/ProspectivePages/ProspectiveCEGEP" component = {ProspectiveCEGEP} />
+           <Route path = "/ProspectivePages/ProspectiveChoosingAMajor" component = {ProspectiveChoosingAMajor} />
+           <Route path = "/ProspectivePages/ProspectiveFreshman" component = {ProspectiveFreshman} />
+           <Route path = "/ProspectivePages/ProspectiveGrad" component = {ProspectiveGrad} />
+           <Route path = "/ProspectivePages/ProspectiveInternship" component = {ProspectiveInternship} />
+           <Route path = "/ProspectivePages/ProspectiveUndergrad" component = {ProspectiveUndergrad} />
+           <Route path = "/ProspectivePages/ProspectiveWhyCS" component = {ProspectiveWhyCS} />
+           <Route path = "/ProspectivePages/ProspectiveTransfer" component = {ProspectiveTransfer} />
+        
+
+         {/*Academic*/}
+           <Route path = "/Academic/AcademicOverview" component = {AcademicOverview} />
+
+          {/*Research*/}
+          <Route path = "/Research/ResearchOverview" component = {ResearchOverview} />
+
+          {/*People*/}
+          <Route path = "/People/PeopleFaculty" component = {PeopleFaculty} />
+
+          {/*News*/}
+          <Route path = "/News/News" component = {News} />
+          
+          {/*Empolyment*/}
+          <Route path = "/Employment/EmploymentFaculty" component = {EmploymentFaculty} />
+
+          {/*Donate*/}
+          <Route path = "/Donate/Donate" component = {Donate} />
+
+           {/*about*/}
+           <Route path = "/About/AboutContacts" component = {AboutContacts} />
+
+       </Switch>
+       <Footer />
+       </Router>
+>>>>>>> Stashed changes
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./CSS/header.css";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Form, Button, FormControl } from "react-bootstrap";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <div className="header">
+          <div className="headerleft">
+            <div
+              class="container-fluid"
+              style={{ backgroundColor: "rgba(212, 57, 57, 0.726)", padding: "20px", marginTop: "100px"}}
+            >
+              <div class="row">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-lg-3"></div>
+                    <div class="col-lg-6">
+                      <div className="logo"></div>
+                    </div>
+                    <div class="col-lg-3"></div>
+                  </div>
+                </div>
+                <div class="container">
+                  <div class="row">
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-10">
+                      <Navbar className="customized-nav" bg="light" expand="lg">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                          <Nav className="mr-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            {/*Prospective*/}
+                            <NavDropdown
+                              title="Prospective"
+                              id="basic-nav-dropdown"
+                            >
+                              <NavDropdown.Item href="/ProspectivePages/ProspectiveGeneralInfo">
+                                General Info
+                              </NavDropdown.Item>
+                              <NavDropdown.Item href="/ProspectivePages/ProspectiveWhyCS">
+                                Why CS
+                              </NavDropdown.Item>
+                              <NavDropdown.Item href="/ProspectivePages/ProspectiveCEGEP">
+                                CEGEP
+                              </NavDropdown.Item>
+                              <NavDropdown.Item href="/ProspectivePages/ProspectiveFreshman">
+                                Freshman
+                              </NavDropdown.Item>
+                              <NavDropdown.Item href="/ProspectivePages/ProspectiveChoosingAMajor">
+                                Choosing A Major
+                              </NavDropdown.Item>
+                              <NavDropdown.Item href="/ProspectivePages/ProspectiveTransfer">
+                                Transfer
+                              </NavDropdown.Item>
+                              <NavDropdown.Item href="/ProspectivePages/ProspectiveInternship">
+                                Internship
+                              </NavDropdown.Item>
+                              <NavDropdown.Divider />
+                              <NavDropdown.Item href="/ProspectivePages/ProspectiveUndergrad">
+                                Undergrad addmission
+                              </NavDropdown.Item>
+                              <NavDropdown.Item href="ProspectiveGrad">
+                                Grad addmission
+                              </NavDropdown.Item>
+                            </NavDropdown>
+                            {/*about */}
+                            <NavDropdown title="About" id="basic-nav-dropdown">
+                              <NavDropdown.Item href="#action/3.2">
+                                Another action
+                              </NavDropdown.Item>
+                              <NavDropdown.Item href="#action/3.3">
+                                Something
+                              </NavDropdown.Item>
+                              <NavDropdown.Divider />
+                              <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                              </NavDropdown.Item>
+                            </NavDropdown>
+                          </Nav>
+                          <Form inline>
+                            <FormControl
+                              type="text"
+                              placeholder="Search"
+                              className="mr-sm-2"
+                            />
+                            <Button variant="outline-danger">Search</Button>
+                          </Form>
+                        </Navbar.Collapse>
+                      </Navbar>
+                    </div>
+                    <div class="col-lg-1"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
