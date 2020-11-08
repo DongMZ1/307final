e32168c990ee562ed2383e9c39a79e38c3b42765
42694647426 <hyyshao@gmail.com>
2020-11-07 20:22:06 -0500

    tables works

--- src/backend/tables.sql
+++ src/backend/tables.sql
@@ -1,12 +1,9 @@
-CREATE DATABASE loginpage;
-USE loginpage;
-
 CREATE TABLE users
 (
     student_id INT NOT NULL,
     email VARCHAR(100) NOT NULL,
     passcode VARCHAR(100) NOT NULL,
-    PRIMARY KEY(pid),
+    PRIMARY KEY(student_id)
 );
 
 INSERT INTO users VALUES (1630081, 'llorman0@baidu.com', 'abc123');
