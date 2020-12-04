9a3125464f6438116d0085b38fb07d22cc8ff875
42694647426 <hyyshao@gmail.com>
2020-12-03 20:12:42 -0500

    donate

--- Client/src/Donate/Donate.js
+++ Client/src/Donate/Donate.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import {Spring} from 'react-spring/renderprops'
+import "../static/bodycomponent.css";
+import DonatePicture from '../pictures/Donate.jpg'
 class Donate extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
     render() { 
+      const backg = {
+
+      }
         return ( 
           <Spring
           from={{ opacity: 0 }}
@@ -13,7 +18,19 @@ class Donate extends Component {
           {props => 
             <div style={props}>       
                 <ScrollToTopOnMount />
-                Donate
+                <div className ="body">
+                <img src={DonatePicture} style ={{padding : "10%"}}></img>
+                <br></br>
+                <h2>Grace Hopper Celebration</h2>
+                <p>Help us send McGill students to the Grace Hopper Celebration of Women in Computing, and in turn build a strong and inclusive community here at McGill University.</p>
+                <p>Computer Science has historically been a predominately male-driven field, but enrolment in the School of Computer Science (SOCS) has tripled since 2009, and over 30% of students are female. Given the steep rise in enrolment at SOCS, The Undergraduate Societies have committed to sending two women to The Grace Hopper Celebration (GHC) of Women in Computing. We hope to increase this impact by sending an additional 6 women to GHC.</p>
+                <p>GHC is the largest gathering of women technologists offering many opportunities for professional networking. Students who attend this conference find it inspiring, and are pushed to finish their degree and implement unique programs such as computing mentorships. The cost for each attendee is $1,500, which includes their accommodation, airfare, and registration.</p>
+                <p>We hope to continue to actively support an inclusive culture of computing at McGill, and empower our female students to build their own networks and meet like-minded individuals. With your help, we can give young female technologists the opportunity to explore the spectrum of all that computer science has to offer and enable these bright young scholars to reach their full potential.</p>
+                <p>To support us and see updates check our Seeds of Change campaign!</p>
+                
+                </div>
+                  
+
                 </div>
                  }
                  </Spring>
