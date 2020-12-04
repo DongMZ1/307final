dec7ddbf4e6c20fa94d29830e6d2c8c8604bcb14
xren935 <xingya.ren@mail.mcgill.ca>
2020-12-04 18:17:21 -0500

    added css to user input fields

--- Client/src/Login/Welcome.js
+++ Client/src/Login/Welcome.js
@@ -5,15 +5,11 @@ import { useHistory } from "react-router-dom";
 import { Session } from 'bc-react-session';
 import { Button } from "react-bootstrap";
 import Axios from "axios";
-import "../static/bodycomponent.css"; 
-import Aos from "aos";
-import "aos/dist/aos.css";
-
+import "../static/bodycomponent.css";
+//import InputField from "./InputField";
 
 const Welcome = () => {
-  Aos.init();
-  //const [staff, setstaff] = useState("false");
-  var staff = "false";
+  
   const { payload } = Session.get();
   const session = Session.get();
   //console.log(session);
@@ -31,6 +27,7 @@ const Welcome = () => {
   }
 
   return (
+
     <Spring
       from={{ opacity: 0 }}
       to={{ opacity: 1 }}>
@@ -40,21 +37,21 @@ const Welcome = () => {
             <ScrollToTopOnMount />
 
           </div>
-          <div class="col-sm-9">
+          <div class="custom-content">
             <div className="body">
               <h1 > Welcome! {username}, You are in your private page. </h1>
               {payload.staff ? (<h2>You logged into a staff account you have right to change content of this page! </h2>)
                 : (<h2>Welcome Student ! </h2>)}
               <p id="usercontent" contenteditable={staff}>{payload.text}</p>
-              <p> Your name is <span id="username" contenteditable={staff}>{payload.name}</span></p>
-              <p> Your age is <span id="userage" contenteditable={staff}>{payload.age}</span></p>
+              <p> Your name is <span className="userInput" id="username" contenteditable={staff}>{payload.name}</span></p>
+              <p> Your age is <span className="userInput" id="userage" contenteditable={staff}>{payload.age}</span></p>
               {payload.staff ? (
                 <>
-                  <Button variant="light" onClick={saveChange}>Save Changes</Button>
+                  <Button class="btn btn-outline-primary" variant="light" onClick={saveChange}>Save Changes</Button>
                   <br />
                   <br />
                   
-              <Button variant="light" onClick={showPage}>{show?(<>Go back</>):(<>I want to Create my own page</>)}</Button>
+              <Button class="btn btn-outline-primary" variant="light" onClick={showPage}>{show?(<>Go back</>):(<>I want to Create my own page</>)}</Button>
               <br/>
               <br/>
               {show?(
@@ -79,26 +76,11 @@ const Welcome = () => {
 
                 </>
               ) : (<></>)}
-        <div style={{position: "relative", display:"block"}}>
-          <ScrollToTopOnMount />
-          <div class="row">
-            <div className ="custom-content" style={{width: "100vw"}}>
-            <h1 > Welcome, {username}! You are in your private page. </h1>
-            {payload.staff ? (<h2>You've logged into a staff account and now have the right to change the content of this page! </h2>)
-              : (<h2>Welcome Student ! </h2>)}
-            <p id="usercontent" contenteditable={staff}>{payload.text}</p>
-            <p> Your name is <span className="userInput" id="username" contenteditable={staff}>{payload.name}</span></p>
-            <p> Your age is <span className="userInput" id="userage" contenteditable={staff}>{payload.age}</span></p>
-            {payload.staff ? (<Button class="btn btn-outline-primary" onClick={saveChange}>Save Changes</Button>)
-              : (<></>)}
             </div>
+
           </div>
-      
-          <div className="background" style={{ background: this.state.active ? this.state.oldColor : this.state.color }} >
-          <div className={this.state.active ? 'overlay active' : 'overlay'} style={{ background: this.state.active ? this.state.color : this.state.oldColor }} />
-          </div>
-        </div>
         </>
+
       }
     </Spring>
   );
@@ -149,8 +131,7 @@ const saveChange = () => {
   var text = document.getElementById('usercontent').innerText;
   var age = parseInt(document.getElementById('userage').innerText);
   var name = document.getElementById('username').innerText;
-  if(Number.isInteger(age) == false){
-    alert("Input age must be an integer!");
+  if (Number.isInteger(age) == false) {
     age = Session.get().payload.age;
     console.log("changed age is still:", age);
     alert("Input age must be integer!");
@@ -176,6 +157,9 @@ const saveChange = () => {
   });
 
 }
+
+
+
 
 export default Welcome;
 
