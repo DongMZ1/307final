75f5fc62335906298608a141ac33b3d9990dc132
42694647426 <hyyshao@gmail.com>
2020-12-10 21:22:22 -0500

    commit

--- Backend/Database/tables.sql
+++ Backend/Database/tables.sql
@@ -16,4 +16,6 @@ CREATE TABLE users
 INSERT INTO users VALUES (1630081, 'student@mcgill.ca', 'John Watson', 'qwert1203asdfg',30, 1, "You can change this content!", NULL);
 INSERT INTO users VALUES (1630082, 'student1@mcgill.ca', 'Jack Watson', 'asdfghj23456',10, 0, "You do not have right to change this content.", NULL);
 
+UPDATE users SET password = ? , Name = ?, age = ? WHERE Username = ?;
+
 SELECT * FROM users;
